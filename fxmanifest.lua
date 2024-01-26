4e2d9797ea6c406dfd7e10ac161bcaa22ae78221
ivanzaida <lurkmorr228@gmail.com>
2024-01-26 16:25:20 +0200

    improve(infrastructure): use github repo name for build

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,7 @@ repository 'https://github.com/ivanzaida/ivz-dev-tools'
 file 'build/ui/**/*'
 
 shared_script 'run.lua'
+ui_page 'build/ui/index.html'
 
 client_script 'build/client.js'
 server_script 'build/server.js'
